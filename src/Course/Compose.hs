2656b3bf205d2fd32a0c3dd29618cca768a7eac7
BarbedWire <barbedwire@BarbedWires-MacBook-Pro.local>
2017-08-02 13:03:47 +0300

    Course.Compose solutions

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -10,26 +10,18 @@ import Course.Monad
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  f <$> Compose cmps = Compose ((<$>) ((<$>) f) cmps)
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
--- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
--- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure = Compose . pure . pure
+  Compose cmpf <*> Compose cmpv = Compose $ lift2 (<*>) cmpf cmpv
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
--- Implement the (=<<) function for a Monad instance for Compose
+
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    error "I don't know how to drill into the innermost monad!!!"
